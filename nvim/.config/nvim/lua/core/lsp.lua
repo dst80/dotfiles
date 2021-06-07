4a4cbf824252c2bfd75cec86a616c772018d38d2
Dennis Stanze <d.stanze@googlemail.com>
2021-06-07 17:41:17 +0200

    move lsp keymaps to lsp.lua

--- nvim/.config/nvim/lua/core/lsp.lua
+++ nvim/.config/nvim/lua/core/lsp.lua
@@ -42,3 +42,24 @@ local opts = {
 }
 
 require('symbols-outline').setup(opts)
+
+local keymap = require('core.keymap_binder')
+-- LSP shortcuts
+keymap.bind("gD"):to_lua_command("vim.lsp.buf.declaration()")
+keymap.bind("gd"):to_lua_command("vim.lsp.buf.definition()")
+keymap.bind("gi"):to_lua_command("vim.lsp.buf.implementation()")
+keymap.bind("gr"):to_lua_command("vim.lsp.buf.references()")
+keymap.bind("<C-K>"):to_lua_command("vim.lsp.buf.hover()")
+keymap.bind("<C-H>"):to_lua_command("vim.lsp.buf.signature_help()")
+keymap.bind("<leader>wa"):to_lua_command("vim.lsp.buf.add_workspace_folder()")
+keymap.bind("<leader>wr"):to_lua_command("vim.lsp.buf.remove_workspace_folder()")
+keymap.bind("<leader>wl"):to_lua_command("print(vim.inspect(vim.lsp.buf.list_workspace_folders()))")
+keymap.bind("<leader>D"):to_lua_command("vim.lsp.buf.type_definition()")
+keymap.bind("<leader>rn"):to_lua_command("vim.lsp.buf.rename()")
+keymap.bind("<leader>ca"):to_lua_command("vim.lsp.buf.code_action()")
+keymap.bind("<leader>sd"):to_lua_command("vim.lsp.diagnostic.show_line_diagnostics()")
+keymap.bind("<leader>gpd"):to_lua_command("vim.lsp.diagnostic.goto_prev()")
+keymap.bind("<leader>gnd"):to_lua_command("vim.lsp.diagnostic.goto_next()")
+keymap.bind("<leader>ll"):to_lua_command("vim.lsp.diagnostic.set_loclist()")
+keymap.bind("<leader>fo"):to_lua_command("vim.lsp.buf.formatting()")
+
