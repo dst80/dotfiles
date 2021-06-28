db9b84b18d1b9c4a48d1bc8913cd4e095ca20f13
Dennis Stanze <d.stanze@googlemail.com>
2021-06-28 21:32:03 +0200

    fix install script

--- nvim/.config/nvim/install_sumneko.sh
+++ nvim/.config/nvim/install_sumneko.sh
@@ -1,10 +1,13 @@
-if [ -d "~/.local/.local/share/nvim/site/pack/lua_language-server" ] 
+cd ~
+sumneko_dir=".local/share/nvim/site/pack/lua_language-server/"
+
+if [ -d "$sumneko_dir" ] 
 then 
-    rm -rf ~/.local/.local/share/nvim/site/pack/lua_language-server
+    rm -rf "$sumneko_dir"
 fi
 
-git clone https://github.com/sumneko/lua-language-server ~/.local/.local/share/nvim/site/pack/lua_language-server
-cd ~/.local/.local/share/nvim/site/pack/lua_language-server
+git clone https://github.com/sumneko/lua-language-server "$sumneko_dir"
+cd "$sumneko_dir"
 git submodule update --init --recursive
 
 cd 3rd/luamake
