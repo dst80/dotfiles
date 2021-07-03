3f70ec42bd35f18e523bcc22e85fbc6aecf82e0d
Dennis Stanze <d.stanze@googlemail.com>
2021-07-03 22:23:05 +0200

    Fix install sumneko script

--- nvim/.config/nvim/install_sumneko.bat
+++ nvim/.config/nvim/install_sumneko.bat
@@ -1,5 +1,5 @@
-git clone https://github.com/sumneko/lua-language-server "%AppData%\nvim-data\site\pack\lua-language-server"
-cd "%AppData%\nvim-data\site\pack\lua-language-server"
+git clone https://github.com/sumneko/lua-language-server "%LocalAppData%\nvim-data\site\pack\lua-language-server"
+cd "%LocalAppData%\nvim-data\site\pack\lua-language-server"
 git submodule update --init --recursive
 
 cd 3rd\luamake
