db4ba28032975310841b82aa4eaf64d758af826f
Dennis Stanze <d.stanze@googlemail.com>
2021-07-18 22:31:40 +0200

    switch to pyright

--- nvim/.config/nvim/lua/configuration/lsp.lua
+++ nvim/.config/nvim/lua/configuration/lsp.lua
@@ -11,7 +11,7 @@ require'lspconfig'.clangd.setup {
     root_dir = function() return vim.loop.cwd() end
 }
 
-require'lspconfig'.pyls.setup {on_attach = on_attach}
+require'lspconfig'.pyright.setup {on_attach = on_attach}
 require'lspconfig'.gopls.setup {on_attach = on_attach}
 require'lspconfig'.rust_analyzer.setup {on_attach = on_attach}
 
