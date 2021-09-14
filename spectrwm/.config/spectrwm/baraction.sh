c3dd537d17de9da317634fa17c341e30b4d8e903
Dennis Stanze <d.stanze@googlemail.com>
2021-09-14 21:48:42 +0200

    update baraction.sh to get a more fancy bar

--- spectrwm/.config/spectrwm/baraction.sh
+++ spectrwm/.config/spectrwm/baraction.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 # baraction.sh for spectrwm status bar
 ## DISK
+
+create_tile () {
+  echo -e "+< +@bg=1; +@fn=1;$1 +@fn=0;$2 +@fg=0;+@bg=0;"
+}
+
+add_spacer () {
+  if [${#1} -eq 1]
+  then
+    echo -e "   "
+  elif [${#1} -eq 2]
+  then
+    echo -e "  "
+  else
+    echo -e ""
+  fi
+}
+
 hdd() {
   hdd="$(df -h | awk 'NR==4{printf "%.2fG %d %", $3, $5}')"
-  echo -e "HDD: $hdd"
+  echo -e "$(create_tile " " "$1$hdd")" 
+
 }
 
 ## RAM
 mem() {
   mem=`free | awk '/Mem/ {printf "%.3fG/%.3fG\n", $3 / 1024.0 / 1024.0, $2 / 1024.0 / 1024.0 }'`
-  echo -e "$mem"
+  echo -e "$(create_tile " " "$1$mem")" 
 }
 
 ## CPU
@@ -20,35 +38,40 @@ cpu() {
   read cpu a b c idle rest < /proc/stat
   total=$((a+b+c+idle))
   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-  echo -e "CPU: $cpu%"
+  echo -e "$(create_tile " " "$1$(add_spacer "$cpu")$cpu %")"
 }
 
 ## VOLUME
 vol() {
     vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
-    echo -e "VOL: $vol"
+    echo -e "$(create_tile " " "$1$vol")" 
 }
 ## Battery 0 Lenovo
 bat0() {
   battery=`acpi | awk '/Battery 0/ {printf "%d%", $4}'`
-  echo -e "Bat0: $battery"
+  if [-n $battery]
+    then
+      echo -e "$(create_tile " " "$1$battery")"
+  fi
 }
 
 ## Battery 1 Lenovo
 bat1() {
   battery=`acpi | awk '/Battery 1/ {printf "%d%", $4}'`
-  echo -e "Bat1: $battery"
+  if [-n $battery]
+    then
+      echo -e "$(create_tile " " "$1$battery")"
+  fi
 }
 
-SLEEP_SEC=3
-#loops forever outputting a line every SLEEP_SEC secs
+get_date () {
+  time_string=`date +'%d %b %Y [%H:%M]'`
+  echo -e "$(create_tile " " "$1$time_string")"
+}
 
-# It seems that we are limited to how many characters can be displayed via
-# the baraction script output. And the the markup tags count in that limit.
-# So I would love to add more functions to this script but it makes the
-# echo output too long to display correctly.
+
+SLEEP_SEC=1
 while :; do
-  echo "+@fg=1;+@fn=0; $(cpu)+@fg=0; |+@fg=2;+@fn=0; $(mem)+@fg=0; |+@fg=3;+@fn=0; $(hdd)+@fg=0; |+@fg=4;+@fn=0;$(vol)+@fg=0; | +@fg=2;+@fn=0;$(bat0)+@fg=0; | +@fg=3;+@fn=0;$(bat1)+@fg=0; |"
-    l
-	sleep $SLEEP_SEC
+  echo "$(cpu "+@fg=1;")$(mem "+@fg=2;")$(hdd "+@fg=3;")$(vol "+@fg=4;")$(bat0 "+@fg=5;")$(bat1 "+@fg=6;")$(get_date "+@fg=5;")"
+  sleep $SLEEP_SEC
 done
