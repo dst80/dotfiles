58b525b9ea8755b62921cb7f0995e3591f34768b
Dennis Stanze <d.stanze@googlemail.com>
2021-09-16 18:22:34 +0200

    make bar more fancy

--- spectrwm/.config/spectrwm/baraction.sh
+++ spectrwm/.config/spectrwm/baraction.sh
@@ -3,7 +3,7 @@
 ## DISK
 
 create_tile () {
-  echo -e "+<+<+@bg=1;+<+@fn=2;+<$1+<+@fn=1;$2+@fg=0;+<+<+@bg=0;"
+  echo -e "+|T+@fg=$3;+@bg=$4;+@fn=1;+<$1+<$2+<"
 }
 
 add_spacer () {
@@ -19,15 +19,15 @@ add_spacer () {
 }
 
 hdd() {
-  hdd=$(df -h | awk 'NR==4{printf "%.2fG %d %", $3, $5}')
-  echo -e "$(create_tile " " "$1$hdd")" 
+  hdd=$(df -h | awk 'NR==4{printf "%.1fG %d %", $3, $5}')
+  echo -e "$(create_tile " " "$hdd" $1 $2)" 
 
 }
 
 ## RAM
 mem() {
-  mem=$(free | awk '/Mem/ {printf "%.3fG/%.3fG\n", $3 / 1024.0 / 1024.0, $2 / 1024.0 / 1024.0 }')
-  echo -e "$(create_tile " " "$1$mem")" 
+  mem=$(free | awk '/Mem/ {printf "%.2fG/%.2fG\n", $3 / 1048576.0, $2 / 1048576.0 }')
+  echo -e "$(create_tile " " "$mem" $1 $2)" 
 }
 
 ## CPU
@@ -38,34 +38,34 @@ cpu() {
   read cpu a b c idle rest < /proc/stat
   total=$((a+b+c+idle))
   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-  echo -e "$(create_tile " " "$1$(add_spacer ${#cpu})$cpu %")"
+  echo -e "$(create_tile " " "$(add_spacer ${#cpu})$cpu %" $1 $2)"
 }
 
 ## VOLUME
 vol() {
-  vol=$(amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g')
-    echo -e "$(create_tile " " "$1$vol")" 
+  vol=$(amixer get Master | awk -F'[][]' 'END{ print $4 }' | sed 's/on://g')
+    echo -e "$(create_tile " " "$vol" $1 $2)" 
 }
 ## Battery 0 Lenovo
 bat0() {
   battery=$(acpi | awk '/Battery 0/ {printf "%d%", $4}');
-  [ ${#battery} -gt 0 ] && echo -e "$(create_tile " " "$1$(add_spacer $battery)$battery")"
+  [ ${#battery} -gt 0 ] && echo -e "$(create_tile " " "$(add_spacer $battery)$battery" $1 $2)"
 }
 
 ## Battery 1 Lenovo
 bat1() {
   battery=$(acpi | awk '/Battery 1/ {printf "%d%", $4}')
-  [ ${#battery} -gt 0 ] && echo -e "$(create_tile " " "$1$(add_spacer $battery)$battery")"
+  [ ${#battery} -gt 0 ] && echo -e "$(create_tile " " "$(add_spacer $battery)$battery" $1 $2)"
 }
 
 get_date () {
-  time_string=$(date +'%d %b %Y [%H:%M]')
-  echo -e "$(create_tile " " "$1$time_string")"
+  time_string=$(date +'%H:%M +|T+@fn=1;%d %b. %Y')
+  echo -e "$(create_tile " " "$time_string" $1 $2)"
 }
 
 
 SLEEP_SEC=1
 while :; do
-  echo "$(cpu "+@fg=1;")$(mem "+@fg=2;")$(hdd "+@fg=3;")$(vol "+@fg=4;")$(bat0 "+@fg=5;")$(bat1 "+@fg=6;")$(get_date "+@fg=5;")"
+  echo "+@bg=0;$(cpu "0" "1")$(mem "0" "2")$(hdd "1" "3")$(vol "1" "4")$(bat0 "1" "5")$(bat1 "1" "5")$(get_date "0" "6")"
   sleep $SLEEP_SEC
 done
