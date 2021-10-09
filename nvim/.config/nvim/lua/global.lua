71ff005dba2ddf469e95e86d020a36f2caf86055
Dennis Stanze <d.stanze@googlemail.com>
2021-10-09 12:39:58 +0200

    fix function call in globalss

--- nvim/.config/nvim/lua/global.lua
+++ nvim/.config/nvim/lua/global.lua
@@ -1,4 +1,4 @@
-fs = require('tools.filesystem')
+local fs = require('tools.filesystem')
 local global = {}
 
 function global:load_variables()
@@ -9,16 +9,16 @@ function global:load_variables()
     self.home = self:get_home_dir()
     self.data_dir = vim.fn.stdpath('data')
     self.vim_path = vim.fn.stdpath('config')
-    self.cache_dir = fs.join_path(self.home, '.cache', 'nvim')
-    self.modules_dir = fs.join_path(vim.fn.stdpath('config'), 'modules')
-    self.site_dir = fs.join_path(vim.fn.stdpath('data'), 'site')
+    self.cache_dir = fs:join_path(self.home, '.cache', 'nvim')
+    self.modules_dir = fs:join_path(vim.fn.stdpath('config'), 'modules')
+    self.site_dir = fs:join_path(vim.fn.stdpath('data'), 'site')
 end
 
 function global:get_home_dir()
     if 'Windows' == fs.system_name then
-        return os.getenv("USERPROFILE")
+        return os.getenv('USERPROFILE')
     end
-    return os.getenv("HOME")
+    return os.getenv('HOME')
 end
 
 global:load_variables()
