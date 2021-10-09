0acca754f8a0a78d551244f4e93f3a1190048550
Dennis Stanze <d.stanze@googlemail.com>
2021-10-09 08:42:35 +0200

    use Noto Sans Nerd Font

--- nvim/.config/nvim/lua/configuration/lualine.lua
+++ nvim/.config/nvim/lua/configuration/lualine.lua
@@ -1,4 +1,11 @@
 require'lualine'.setup {
-    options = {lower = true, theme = 'nord'},
-    sections = {lualine_a = {{'mode', lower = false}}, lualine_b = {'branch'}}
+    options = {lower = true, theme = 'nord', icons_enabled = 1},
+    sections = {
+        lualine_a = {{'mode', lower = false}},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    }
 }
