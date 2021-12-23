d673dfac3fcd822e06df6007b60c3d1e8c1c99d6
Dennis Stanze <d.stanze@googlemail.com>
2021-12-23 13:13:37 +0100

    update general keymap of nvim for neo layout

--- nvim/.config/nvim/lua/configuration/keymap.lua
+++ nvim/.config/nvim/lua/configuration/keymap.lua
@@ -1,24 +1,39 @@
 local keymap = require("tools.keymap")
 
+-- neo keyboard
 -- window controls
-keymap.bind("<C-l>"):in_mode("n"):to_command("<C-w>l")
-keymap.bind("<C-h>"):in_mode("n"):to_command("<C-w>h")
-keymap.bind("<C-j>"):in_mode("n"):to_command("<C-w>j")
-keymap.bind("<C-k>"):in_mode("n"):to_command("<C-w>k")
+keymap.bind("<C-Left>"):in_mode("n"):to_command("<C-w>l")
+keymap.bind("<C-Right>"):in_mode("n"):to_command("<C-w>h")
+keymap.bind("<C-Down>"):in_mode("n"):to_command("<C-w>j")
+keymap.bind("<C-Up>"):in_mode("n"):to_command("<C-w>k")
+-- line mover commands
+keymap.bind("<A-Down>"):in_mode("n"):to_command(":m .+1<CR>==")
+keymap.bind("<A-Up>"):in_mode("n"):to_command(":m .-2<CR>==")
+keymap.bind("<A-Down>"):in_mode("i"):to_command("<Esc>:m .+1<CR>==gi")
+keymap.bind("<A-Up>"):in_mode("i"):to_command("<Esc>:m .-2<CR>==gi")
+keymap.bind("<A-Down>"):in_mode("v"):to_command(":m '>+1<CR>gv=gv")
+keymap.bind("<A-Up>"):in_mode("v"):to_command(":m '<-2<CR>gv=gv")
+
+-- qwerty
+-- -- window controls
+-- keymap.bind("<C-l>"):in_mode("n"):to_command("<C-w>l")
+-- keymap.bind("<C-h>"):in_mode("n"):to_command("<C-w>h")
+-- keymap.bind("<C-j>"):in_mode("n"):to_command("<C-w>j")
+-- keymap.bind("<C-k>"):in_mode("n"):to_command("<C-w>k")
+-- -- line mover commands
+-- keymap.bind("<A-j>"):in_mode("n"):to_command(":m .+1<CR>==")
+-- keymap.bind("<A-k>"):in_mode("n"):to_command(":m .-2<CR>==")
+-- keymap.bind("<A-j>"):in_mode("i"):to_command("<Esc>:m .+1<CR>==gi")
+-- keymap.bind("<A-k>"):in_mode("i"):to_command("<Esc>:m .-2<CR>==gi")
+-- keymap.bind("<A-j>"):in_mode("v"):to_command(":m '>+1<CR>gv=gv")
+-- keymap.bind("<A-k>"):in_mode("v"):to_command(":m '<-2<CR>gv=gv")
+
+
 keymap.bind("<leader>-"):in_mode("n"):to_command(":vertical resize -5<CR>")
 keymap.bind("<leader>+"):in_mode("n"):to_command(":vertical resize +5<CR>")
--- line mover commands
-keymap.bind("<A-j>"):in_mode("n"):to_command(":m .+1<CR>==")
-keymap.bind("<A-k>"):in_mode("n"):to_command(":m .-2<CR>==")
-keymap.bind("<A-j>"):in_mode("i"):to_command("<Esc>:m .+1<CR>==gi")
-keymap.bind("<A-k>"):in_mode("i"):to_command("<Esc>:m .-2<CR>==gi")
-keymap.bind("<A-j>"):in_mode("v"):to_command(":m '>+1<CR>gv=gv")
-keymap.bind("<A-k>"):in_mode("v"):to_command(":m '<-2<CR>gv=gv")
 -- shortcuts for save and close
 keymap.bind("<C-q>"):in_mode("n"):to_command(":wq<CR>")
 keymap.bind("<C-s>"):in_mode("n"):to_command(":w<CR>")
 keymap.bind("<C-q>"):in_mode("i"):to_command("<Esc>:wq<CR>")
 keymap.bind("<C-s>"):in_mode("i"):to_command("<Esc>:w<CR>")
 keymap.bind("<leader><CR>"):in_mode('n'):to_command(":noh<CR>")
--- disable arrow keys
-keymap.deactivate_keys({"<Left>", "<Right>", "<Up>", "<Down>"})
