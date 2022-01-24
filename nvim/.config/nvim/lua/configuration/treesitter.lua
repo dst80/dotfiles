69059182d418536e662b7eec041ac323bac45722
dst80 <d.stanze@googlemail.com>
2022-01-24 21:12:01 +0100

    define treesitter build tools

--- nvim/.config/nvim/lua/configuration/treesitter.lua
+++ nvim/.config/nvim/lua/configuration/treesitter.lua
@@ -1,3 +1,5 @@
+require'nvim-treesitter.install'.compilers = { "clang" }
+
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {
         "c", "cpp", "c_sharp", "css", "scss", "html", "javascript", "java",
