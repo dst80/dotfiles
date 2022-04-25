0f5dc88d3f088db12e804b4a91a4e91d50b6b5f4
dst80 <d.stanze@googlemail.com>
2022-04-26 00:56:29 +0200

    fix empty query fail for commands

--- bin/.local/bin/cheatsheet.sh
+++ bin/.local/bin/cheatsheet.sh
@@ -7,23 +7,24 @@ fi
 read -p "Enter Query: " query
 
 if [ "$TMUX" ]; then
-      
-
-    run()
-    {
+    run() { 
       tmux neww "eval $1" 
     }
 else
-    run()
-    {
-      eval $1
+    run() { 
+      eval $1 
     }
 fi
 
+if [ ! -z "$selected" ]; then
+  run "curl -s cht.sh/$selected | less -r"
+  exit 0
+fi
 
 if grep -qs "$selected" ~/.config/cheatsheet/languages; then
     query=`echo $query | tr ' ' '+'`
     run "curl -s cht.sh/$selected/$query | less -r"
-  else
-    run "curl -s cht.sh/$selected~$query | less -r"
+    exit 0
 fi
+
+run "curl -s cht.sh/$selected~$query | less -r"
