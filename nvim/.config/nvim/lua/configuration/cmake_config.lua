f6c2d2f71e9db326ba8d4c18e6eed0e082f5b0df
dst80 <d.stanze@googlemail.com>
2022-04-25 21:22:03 +0200

    update cmake configuration for nvim after breaking change

--- nvim/.config/nvim/lua/configuration/cmake_config.lua
+++ nvim/.config/nvim/lua/configuration/cmake_config.lua
@@ -1,20 +1,21 @@
 local keymap = require("tools.keymap")
 local Path = require ('plenary.path')
 require('cmake').setup({
-  parameters_file = 'neovim.json',
+  parameters_file = 'build/nvim_cmake_configuration.json',
   build_dir = tostring(Path:new('{cwd}', 'build', '{os}-{build_type}')),
   default_projects_path = tostring(Path:new(vim.loop.os_homedir(), 'repos/projects')),
   configure_args = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' },
   build_args = {},
   quickfix_height = 10,
-  dap_configuration = { type = 'cpp', request = 'launch' },
+  dap_configuration = { type = 'lldb', request = 'launch' },
   dap_open_command = require('dap').repl.open,
 })
 
-keymap.bind("<leader>ccp"):to_command(":Telescope cmake create_project")
-keymap.bind("<leader>cst"):to_command(":Telescope cmake select_target")
-keymap.bind("<leader>csb"):to_command(":Telescope cmake select_build_type")
-keymap.bind("<leader>cc"):to_command(":CMake configure")
-keymap.bind("<leader>cb"):to_command(":CMake build")
-keymap.bind("<leader>cr"):to_command(":CMake run")
-keymap.bind("<leader>ccl"):to_command(":CMake clean")
+keymap.bind("<leader>ccp"):in_mode('n'):to_command(":CMake create_project<CR>")
+keymap.bind("<leader>cst"):in_mode('n'):to_command(":CMake select_target<CR>")
+keymap.bind("<leader>csb"):in_mode('n'):to_command(":CMake select_build_type<CR>")
+keymap.bind("<leader>cc"):in_mode('n'):to_command(":CMake configure<CR>")
+keymap.bind("<leader>cb"):in_mode('n'):to_command(":CMake build<CR>")
+keymap.bind("<leader>cr"):in_mode('n'):to_command(":CMake run<CR>")
+keymap.bind("<leader>ccl"):in_mode('n'):to_command(":CMake clean<CR>")
+keymap.bind("<leader>cdbg"):in_mode('n'):to_command(":CMake clean<CR>")
