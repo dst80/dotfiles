91b1689a130dd5883604ea99b87ef219b185c9cf
dst80 <d.stanze@googlemail.com>
2022-05-24 21:34:31 +0200

    choose new keymaps for completion

--- nvim/.config/nvim/lua/configuration/completion.lua
+++ nvim/.config/nvim/lua/configuration/completion.lua
@@ -7,47 +7,25 @@ cmp.setup({
     end,
   },
   mapping = {
-    ['<TAB>'] = cmp.mapping.select_next_item(),
-    ['<S-TAB>'] = cmp.mapping.select_prev_item(),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-y>'] = cmp.config.disable,
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-y>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+        },
   },
   sources = cmp.config.sources({
+    { name = 'nvim_lua' },
     { name = 'nvim_lsp' },
     { name = 'snippy' },
-  }, {
-    { name = 'buffer' },
-  })
+    { name = 'buffer', keyword_length = 5 },
+  }),
+  experimental = {
+        native_menu = false,
+        ghost_text = true
+    },
 })
 
-cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-    { name = 'buffer' }
-  }
-})
-
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
-
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
--- require('lspconfig').tsserver.setup {capabilities = capabilities}
--- require('lspconfig').clangd.setup {capabilities = capabilities}
--- require('lspconfig').pyright.setup {capabilities = capabilities}
--- require('lspconfig').gopls.setup {capabilities = capabilities}
--- require('lspconfig').rust_analyzer.setup {capabilities = capabilities}
--- require('lspconfig').sumneko_lua.setup {capabilities = capabilities}
