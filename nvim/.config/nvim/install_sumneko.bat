ad43090075613fdda31245791e555d2b5bb83f17
Dennis Stanze <d.stanze@googlemail.com>
2022-06-08 21:14:00 +0200

    remove unused scripts and submodules

--- nvim/.config/nvim/install_sumneko.bat
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/sumneko/lua-language-server "%LocalAppData%\nvim-data\site\pack\lua-language-server"
-cd "%LocalAppData%\nvim-data\site\pack\lua-language-server"
-git submodule update --init --recursive
-
-cd 3rd\luamake
-compile\install.bat
-cd ..\..
-3rd\luamake\luamake.exe rebuild
