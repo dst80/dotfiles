6cb8c785b16f1c8989902abf8f699c2b966cb8de
Dennis Stanze <d.stanze@googlemail.com>
2022-06-20 20:17:21 +0200

    update cmake settings for neovim-cmake

--- nvim/.config/nvim/lua/configuration/cmake_config.lua
+++ nvim/.config/nvim/lua/configuration/cmake_config.lua
@@ -1,16 +1,27 @@
-local Path = require ('plenary.path')
+local Path = require('plenary.path')
+
 require('cmake').setup({
-  parameters_file = 'build/nvim_cmake_configuration.json',
-  build_dir = tostring(Path:new('{cwd}', 'build', '{os}-{build_type}')),
-  default_projects_path = tostring(Path:new(vim.loop.os_homedir(), 'repos/projects')),
-  configure_args = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' },
-  build_args = {},
-  quickfix_height = 10,
-  dap_configuration = { type = 'lldb', request = 'launch' },
-  dap_open_command = require('dap').repl.open,
+    parameters_file = 'build/nvim_cmake_configuration.json',
+    default_projects_path = tostring(Path:new(vim.loop.os_homedir(), 'repos/projects')),
+    build_dir = tostring(Path:new('{cwd}', 'build', '{os}-{build_type}')),
+    configure_args = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' },
+    copy_compile_commands = true,
+    build_args = {},
+    quickfix = {
+        pos = 'botright',
+        height = 10,
+        only_on_error = false,
+    },
+    dap_configuration = {
+        type = 'lldb',
+        request = 'launch',
+        stopOnEntry = false,
+        runInTerminal = false,
+    },
+    dap_open_command = require('dap').repl.open,
 })
 
-local options = {noremap = true, silent = true}
+local options = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>ccp", ":CMake create_project<CR>", options)
 vim.keymap.set("n", "<leader>cst", ":CMake select_target<CR>", options)
 vim.keymap.set("n", "<leader>csb", ":CMake select_build_type<CR>", options)
