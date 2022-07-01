f2900450572db31aa4883d29e648244287b452fe
dst80 <d.stanze@googlemail.com>
2022-07-01 19:15:52 +0200

    reduce redundancy of rust snippets

--- nvim/.config/nvim/lua/configuration/luasnip/filetypes/rust.lua
+++ nvim/.config/nvim/lua/configuration/luasnip/filetypes/rust.lua
@@ -20,30 +20,7 @@ local on = function(trigger)
 end
 
 luasnip.add_snippets("rust", {
-    --type structure
-    snippet(on("struct"),
-        format("struct {} {{\n\t{}\n}}", {
-            insert(1, "Name"),
-            insert(0)
-        })),
-    snippet(on("let"),
-        format("let {} = {}", {
-            insert(1, "param"),
-            insert(0, "object")
-        })),
-    snippet(on("enum"),
-        format("enum {} {{\n\t{}\n}}", {
-            insert(1, "Name"),
-            insert(0),
-        })),
-    snippet(on("params"),
-        format("{} : {}", {
-            insert(1, "param"),
-            insert(2, "Type")
-        })),
     -- function closure, traits
-    snippet(on("main"),
-        format("fn main() {{\n\t{}\n}}", insert(0))),
     snippet(on("fn"),
         format("fn {}({}) {}{{\n\t{}\n}}", {
             insert(1, "name"),
@@ -67,68 +44,12 @@ luasnip.add_snippets("rust", {
             same_as(2),
             insert(0, "")
         })),
-    snippet(on("trait"),
-        format("trait {} {{\n\t{}\n}}", {
-            insert(1, "Name"),
-            insert(0)
+    snippet(on("extern"),
+        format("extern {}", choice(1) {
+            format("crate {};", insert(1, "some_crate")),
+            format("\"C\" {}", insert(1, "")),
         })),
-    snippet(on("impl"),
-        format("impl {} for {} {{\n\t{}\n}}", {
-            insert(1, "Trait"),
-            insert(2, "Name"),
-            insert(0)
-        })),
-
-    --loops
-    snippet(on("for"),
-        format("for {} in {} {{\n\t{}\n}}", {
-            insert(1),
-            insert(2),
-            insert(0)
-        })),
-    snippet(on("while"),
-        format("while {} {{\n\t{}\n}}", {
-            insert(1, ""),
-            insert(0, "")
-        })),
-
-    -- conditional branching
-    snippet(on("if"),
-        format("if {} {{\n\t{}\n}}", {
-            insert(1, ""),
-            insert(0)
-        })),
-    snippet(on("else"),
-        format("else {}{{\n\t{}\n}}", {
-            insert(1, ""),
-            insert(0)
-        })),
-    snippet(on("match"),
-        format("match {} {{\n\t{}}};", {
-            insert(1, "matcher"),
-            insert(0)
-        })),
-    -- unsafe
-    snippet(on("unsafe"),
-        format("unsafe {{\n\t{}\n}}", insert(0))),
-
     -- Result and error
-    snippet(on("Result"),
-        format("Result<{}, {}>{}", {
-            insert(1, "Type"),
-            insert(2, "Error"),
-            insert(0)
-        })),
-    snippet(on("Ok"),
-        format("Ok({}){},", {
-            insert(1, "value"),
-            insert(0)
-        })),
-    snippet(on("Err"),
-        format("Err({}){}", {
-            insert(1, "err"),
-            insert(0)
-        })),
     snippet(on("matchresult"),
         format("match {} {{\n\tOk({}) => {},\n\t{}{}\n}}", {
             insert(1, "Result"),
@@ -154,94 +75,6 @@ luasnip.add_snippets("rust", {
                     same_as(1),
                 })
             }),
-            insert(0)
-        })),
-
-    --smart pointer creation
-    snippet(on("box"),
-        format("Box:new({}){}", {
-            insert(1),
-            insert(0)
-        })),
-    snippet(on("rc"),
-        format("Rc:new({}){}", {
-            insert(1),
-            insert(0)
-        })),
-    snippet(on("arc"),
-        format("Arc:new({}){}", {
-            insert(1),
-            insert(0)
-        })),
-    snippet(on("cell"),
-        format("Cell::new({}){}", {
-            insert(1),
-            insert(0)
-        })),
-    snippet(on("refcell"),
-        format("RefCell::new({}){}", {
-            insert(1),
-            insert(0)
-        })),
-    --attributeglobal_allocator
-    snippet(on("derive"),
-        format("#[derive({})]{}", {
-            insert(1, "PartialEq"),
-            insert(0),
-        })),
-
-    snippet(on("glballo"),
-        format("#[GlobalAllocator]{}", {
-            insert(0),
-        })),
-    --common traits
-    snippet(on("partialeq"),
-        format("impl PartialEq for {} {{\n\tfn eq(&self, other: &self) -> bool {{\n\t{}\n\t}}\n}}", {
-            insert(1, "Type"),
-            insert(0)
-        })),
-    snippet(on("ord"),
-        format("impl Ord for {} {{\n\tfn cmp(&self, other: &self) -> Ordering {{\n\t{}\n\t}}\n}}", {
-            insert(1, "Type"),
-            insert(0)
-        })),
-    snippet(on("parialord"),
-        format("impl PartialOrd for {} {{\n\tfn partial_cmp(&self, other: &self) -> Option<Ordering> {{\n\t{}\n\t}}\n}}", {
-            insert(1, "Type"),
-            insert(0)
-        })),
-        snippet(on("drop"),
-        format("impl Drop for {} {{\n\tfn drop(&mut self) {{\n\t{}\n\t}}\n}}", {
-            insert(1, "Type"),
-            insert(0)
-        })),
-    snippet(on("asref"),
-        format("impl AsRef<{}> for {} {{\n\tfn as_ref(&self) -> &{} {{\n\t&self.{}\n\t}}\n}}", {
-            insert(1, "Ref"),
-            insert(2, "Type"),
-            same_as(1),
-            insert(0, "a_field")
-        })),
-    snippet(on("asmut"),
-        format("impl AsMut<{}> for {} {{\n\tfn as_mut(&self) -> &mut {} {{\n\t&mut self.{}\n\t}}\n}}", {
-            insert(1, "Ref"),
-            insert(2, "Type"),
-            same_as(1),
-            insert(0, "a_field")
-        })),
-    --testing
-    snippet(on("modtest"),
-        format("#[cfg(test)]\nmod test {{\n\tuse super::*;\n{}\n}}", insert(0))),
-    snippet(on("test"),
-        format("#[test]\nfn {}() {} {{\n\t{}\n}}", {
-            insert(1, "testname"),
-            insert(2, ""),
-            insert(0)
-        })),
-    snippet(on("assert_eq"),
-        format("assert_eq!({}, {});{}", {
-            insert(1),
-            insert(2),
             insert(0)
         })),
 })
--- nvim/.config/nvim/lua/configuration/luasnip/init.lua
+++ nvim/.config/nvim/lua/configuration/luasnip/init.lua
@@ -22,12 +22,6 @@ luasnip.config.setup({
     },
 })
 
--- require('luasnip.loaders.from_snipmate').lazy_load({
---     paths = {
---         '~/.config/nvim/snippets'
---     }
--- })
-
 require('luasnip.loaders.from_vscode').lazy_load({
     paths = {
         "~/.config/snippets/vsc-snippets"
