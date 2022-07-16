9589f97ff830dd1eba03000aee5c4bbd18ea2ab5
dst80 <d.stanze@googlemail.com>
2022-07-16 12:22:35 +0200

    add JetBrains Mono to fonts and set the fonts in config files

--- nvim/.config/nvim/lua/configuration/lualine.lua
+++ nvim/.config/nvim/lua/configuration/lualine.lua
@@ -1,11 +1,23 @@
-require'lualine'.setup {
-    options = {lower = true, theme = 'nord', icons_enabled = 1},
+require 'lualine'.setup {
+    options = {
+        lower = true,
+        theme = 'nord',
+        icons_enabled = 1,
+        component_separators = {
+            left = '',
+            right = ''
+        },
+        section_separators = {
+            left = '',
+            right = ''
+        }
+    },
     sections = {
-        lualine_a = {{'mode', lower = false}},
-        lualine_b = {'branch'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_a = { { 'mode', lower = false } },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
     }
 }
