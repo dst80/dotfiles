ba829ab91d7c4a6d6fd6d66c5c505cdf8d285a39
dst80 <d.stanze@googlemail.com>
2022-09-30 22:36:40 +0200

    extend treesitter setting

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,18 +1,19 @@
-require'nvim-treesitter.install'.compilers = { "clang" }
+require 'nvim-treesitter.install'.compilers = { "clang" }
 
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
     ensure_installed = {
-        "c", "cpp", "c_sharp", "css", "scss", "html", "javascript", "java",
-        "lua", "rust", "go",
+        "bash", "c", "cpp", "c_sharp", "css", "scss", "html", "javascript",
+        "java", "json", "lua", "rust", "go", "typescript", "markdown", "python",
+        "vue", "graphql", "glsl", "make", "toml", "yaml",
     },
     ignore_install = {
         "beancount", "bibtex", "dart", "devicetree", "dot", "elixir", "elm",
         "erlang", "fennel", "fish", "fortran", "Godot", "Glimmer and Ember",
-        "Godot Resources", "gomod", "gdscript", "graphql", "haskell", "hcl",
+        "Godot Resources", "gomod", "gdscript", "haskell", "hcl",
         "heex", "ledger", "nix", "ocaml", "ocaml_interface", "ocamllex", "php",
         "pioasm", "ql", "r", "rst", "scala", "sparql", "supercollider",
-        "surface", "svelte", "teal", "tlaplus", "toml", "turtle", "verilog",
-        "vue", "yang", "zig"
+        "surface", "svelte", "teal", "tlaplus", "turtle", "verilog",
+        "yang"
     },
     incremental_selection = {
         enable = true,
@@ -23,11 +24,15 @@ require'nvim-treesitter.configs'.setup {
             node_decremental = "grm"
         }
     },
-    indent = {enable = true},
+    indent = { enable = true },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     }
 }
 
-vim.keymap.set("n", "<leader>rts", ":write | edit | TSBufEnable highlight<CR>", {silent = true, noremap = true})
+vim.keymap.set(
+    "n",
+    "<leader>rts",
+    ":write | edit | TSBufEnable highlight<CR>",
+    { silent = true, noremap = true })
