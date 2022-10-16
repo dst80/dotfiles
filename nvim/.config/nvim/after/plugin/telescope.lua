571fec951cabfbe944480ca6c4f5e10e96fb6cd6
Dennis Stanze <d.stanze@googlemail.com>
2022-10-16 14:30:30 +0200

    optimize fuzzy find

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -45,28 +45,48 @@ require('telescope').load_extension('dotfiles')
 require('telescope').load_extension('file_browser')
 require('telescope').load_extension('ui-select')
 
+
+local path = require('plenary.path')
+local function find_git_repo_or_use_current_dir(current_path)
+    local root_dir = current_path
+    for dir in vim.fs.parents(current_path) do
+        if (vim.fn.isdirectory(tostring(path:new(dir, "/.git"))) == 1) then
+            root_dir = dir
+            break
+        end
+    end
+    return root_dir
+end
+
 local options = { silent = true, noremap = true }
-vim.keymap.set("n", "<leader>gf", require('telescope.builtin').git_files, options)
-vim.keymap.set("n", "<leader>ff", require('telescope.builtin').find_files, options)
-vim.keymap.set("n", "<leader>fg", require('telescope.builtin').live_grep, options)
 vim.keymap.set("n", "<leader>fb", require('telescope').extensions.file_browser.file_browser, options)
 vim.keymap.set("n", "<leader>h", require('telescope.builtin').help_tags, options)
-vim.keymap.set("n", "<leader>dt", require('telescope.builtin').diagnostics, options)
-
+vim.keymap.set("n", "<leader>fd", require('telescope.builtin').diagnostics, options)
 vim.keymap.set("n", "<leader>sb", require('telescope.builtin').buffers, options)
 
-local path = require ('plenary.path')
-vim.keymap.set("n", "<leader>cfg", function()
-    require('telescope.builtin').find_files({ cwd = tostring(path:new(vim.fn.stdpath('config'))) })
+vim.keymap.set("n", "<leader>ff", function()
+    require('telescope.builtin').find_files({
+        cwd = find_git_repo_or_use_current_dir(vim.fn.getcwd()),
+        hidden = false
+    })
+end, options)
+
+vim.keymap.set("n", "<leader>fhf", function()
+    require('telescope.builtin').find_files({
+        cwd = find_git_repo_or_use_current_dir(vim.fn.getcwd()),
+        hidden = true,
+    })
 end, options)
 
 vim.keymap.set("n", "<leader>df", function()
-    require('telescope.builtin').git_files({ cwd = tostring(path:new(vim.fn.getenv('HOME'), 'dotfiles')) })
+    require('telescope.builtin').find_files({
+        cwd = tostring(path:new(vim.fn.getenv('HOME'), 'dotfiles')),
+        hidden = true
+    })
 end, options)
 
-vim.keymap.set("n", "<leader>fs", function()
-    require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For->") })
-end, options)
+
+vim.keymap.set("n", "<leader>fg", require('telescope.builtin').live_grep, options)
 
 vim.keymap.set("n", "<leader>fw", function()
     require('telescope.builtin').grep_string({ search = vim.fn.expand("<cword>") })
