8c54cd06d495135988020a9c80595c8e1f279d50
dst80 <d.stanze@googlemail.com>
2022-12-08 23:40:13 +0100

    add some new linter

--- nvim/.config/nvim/lua/configuration/lsp/null-lsp.lua
+++ nvim/.config/nvim/lua/configuration/lsp/null-lsp.lua
@@ -4,8 +4,12 @@ M.setup = function(_)
     local has_null_lsp, null_lsp = pcall(require, "null-lsp")
     if has_null_lsp then
         null_lsp.setup({
-            null_lsp.buildin.formatting.stylua,
-            null_lsp.buildin.formatting.prettierd,
+            null_lsp.buildins.formatting.stylua,
+            null_lsp.buildins.formatting.prettierd,
+            null_lsp.buildins.diagnostics.clang_format,
+            null_lsp.buildins.diagnostics.cmake_format,
+            null_lsp.buildins.diagnostics.cmake_lint,
+            null_lsp.buildins.diagnostics.write_good,
         })
     end
 end
