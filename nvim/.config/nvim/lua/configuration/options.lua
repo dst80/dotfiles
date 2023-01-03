cd20f45aaa8f774e9d4ef8e58984e56b915e53fb
Dennis Stanze <d.stanze@googlemail.com>
2023-01-03 17:29:01 +0100

    fix windows undo directory

--- nvim/.config/nvim/lua/configuration/options.lua
+++ nvim/.config/nvim/lua/configuration/options.lua
@@ -55,7 +55,13 @@ vim.opt.writebackup = false
 vim.opt.swapfile = false
 vim.opt.history = 2000
 vim.opt.undofile = true
-vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+
+if (vim.loop.os_uname().sysname == "Windows_NT") then
+    vim.opt.undodir = os.getenv("USERPROFILE") .. "/.cache/nvim/undodir"
+else
+    vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+end
+
 vim.opt.shada = "!,'300,<50,@100,s10,h"
 vim.opt.backupskip = "/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,.vault.vim"
 
