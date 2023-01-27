99c85ee9d061cfbbdb6269d093efd09a85f4cd59
dst80 <d.stanze@googlemail.com>
2023-01-27 22:37:55 +0100

    switch to default bootstrap

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,10 +1,13 @@
-if not pcall(require, "packer") then
-    vim.cmd(
-        "!git clone https://github.com/wbthomason/packer.nvim " ..
-        vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    )
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
-
 
 return require("packer").startup(
     function(use)
