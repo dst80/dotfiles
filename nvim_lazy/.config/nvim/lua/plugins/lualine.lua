be8f3f870082285c4ed5a39824f3b04852621ad2
dst80 <d.stanze@googlemail.com>
2023-02-04 13:24:47 +0100

    switch to onenord

--- nvim_lazy/.config/nvim/lua/plugins/lualine.lua
+++ nvim_lazy/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 local M = {
     "hoob3rt/lualine.nvim",
     dependencies = {
-        "navarasu/onedark.nvim",
+        'rmehri01/onenord.nvim',
     },
 }
 
--- nvim_lazy/.config/nvim/lua/plugins/theme.lua
+++ nvim_lazy/.config/nvim/lua/plugins/theme.lua
@@ -1,75 +1,46 @@
 local M = {
-    "navarasu/onedark.nvim",
+    'rmehri01/onenord.nvim',
 }
 
 function M.config()
-    local onedark = require("onedark")
+    local onedark = require('onenord')
     onedark.setup({
-        -- Main options --
-        style = 'dark',
-        transparent = false,
-        term_colors = true,
-        ending_tildes = true,
-        cmp_itemkind_reverse = false,
-
-        -- toggle theme style ---
-        toggle_style_key = "<leader>ß,",
-        toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
-
-        code_style = {
-            comments = 'italic',
-            keywords = 'bold',
-            functions = 'none',
-            strings = 'none',
-            variables = 'none'
+        theme = "dark",
+        borders = true,
+        fade_nc = false,
+        styles = {
+            comments = "NONE",
+            strings = "NONE",
+            keywords = "NONE",
+            functions = "NONE",
+            variables = "NONE",
+            diagnostics = "underline",
         },
-
-        lualine = {
-            transparent = false,
+        disable = {
+            background = false,
+            cursorline = false,
+            eob_lines = true,
         },
-        -- onedark to nord
-        colors = {
-            black = "#2e3440",
-            bg0 = "#2e3440",
-            bg1 = "#323947",
-            bg2 = "#3b4252",
-            bg3 = "#4c566a",
-            bg_d = "#21252b",
-            bg_blue = "#5e81ac",
-            bg_yellow = "#ebd09c",
-            fg = "#eceff4",
-            purple = "#b48ead",
-            green = "#a3be8c",
-            orange = "#d5937e",
-            blue = "#81a1c1",
-            yellow = "#ebcb8b",
-            cyan = "#88c0d0",
-            red = "#bf616a",
-            grey = "#4c566a",
-            light_grey = "#d8dee9",
-            dark_cyan = "#2b6f77",
-            dark_red = "#52737d",
-            dark_yellow = "#a58e61",
-            dark_purple = "#90728a",
-            dark_blue = "#5e81ac",
-            diff_add = "#31392b",
-            diff_delete = "#382b2c",
-            diff_change = "#1c3448",
-            diff_text = "#2c5372",
-        }, -- Override default colors
-        highlights = {
-            IncSearch = { bg = "#5e81ac" },
-            --["@type"] = { fg = "#D9A0A6", fmt = "italic" },
-            ["@constructor"] = { fg = "#8EA7C5", fmt = "none" },
-        }, -- Override highlight groups
-
-        diagnostics = {
-            darker = false, -- darker colors for diagnostic
-            undercurl = true, -- use undercurl instead of underline for diagnostics
-            background = false, -- use background color for virtual text
+        inverse = {
+            match_paren = false,
+        },
+        custom_highlights = {
+            Cursorline = { bg = "#292f3a" },
+            CursorColumn = { bg = "#292f3a" },
+            ColorColumn = { bg = "#292f3a" },
+            Search = { fg = "#ebcb8b", bg = "#4c566a", style = "bold" },
+            IncSearch = { bg = "#4c566a", style = "bold" },
+            MatchParen = { bg = "#4c566a", style = "bold" },
+            WildMenu = { bg = "#4c566a", style = "bold" },
+            ["@constructor"] = { fg = "#D9A0A6", fmt = "italic" },
+            ["@type"] = { fg = "#8EA7C5", fmt = "none" },
+        },
+        custom_colors = {
+            warn = "#ebcb8b"
         },
     })
-    require('onedark').load()
+
+    vim.cmd("colorscheme onenord")
 end
 
 return M
