1d3b30ad2ff2662fea98e2473eb0d2ff13334693
dst80 <d.stanze@googlemail.com>
2023-02-09 13:05:55 +0100

    use nordfox theme

--- nvim_lazy/.config/nvim/lua/plugins/theme.lua
+++ nvim_lazy/.config/nvim/lua/plugins/theme.lua
@@ -1,46 +1,18 @@
 local M = {
-    'rmehri01/onenord.nvim',
+    "EdenEast/nightfox.nvim",
 }
 
 function M.config()
-    local onedark = require('onenord')
-    onedark.setup({
-        theme = "dark",
-        borders = true,
-        fade_nc = false,
-        styles = {
-            comments = "NONE",
-            strings = "NONE",
-            keywords = "NONE",
-            functions = "NONE",
-            variables = "NONE",
-            diagnostics = "underline",
-        },
-        disable = {
-            background = false,
-            cursorline = false,
-            eob_lines = true,
-        },
-        inverse = {
-            match_paren = false,
-        },
-        custom_highlights = {
-            CursorLine = { bg = "#292f3a" },
-            CursorColumn = { bg = "#292f3a" },
-            ColorColumn = { bg = "#292f3a" },
-            Search = { fg = "#ebcb8b", bg = "#4c566a", style = "bold" },
-            IncSearch = { bg = "#4c566a", style = "bold" },
-            MatchParen = { bg = "#4c566a", style = "bold" },
-            WildMenu = { bg = "#4c566a", style = "bold" },
-            ["@constructor"] = { fg = "#D9A0A6", fmt = "italic" },
-            ["@type"] = { fg = "#8EA7C5", fmt = "none" },
-        },
-        custom_colors = {
-            warn = "#ebcb8b"
-        },
+    local nightfox = require("nightfox")
+    local shade = require("nightfox.lib.shade")
+    nightfox.setup({
+        palette = {
+            nordfox = {
+                white = shade.new("#e5e0f0", "#eceff4", "#d8dee9")
+            }
+        }
     })
-
-    vim.cmd("colorscheme onenord")
+    vim.cmd("colorscheme nordfox")
 end
 
 return M
