b7e9a33b2ab66db3a32d58856156d5c20cccd331
Dennis Stanze <d.stanze@googlemail.com>
2023-02-11 19:01:06 +0100

    match leap indicator to nord theme

--- nvim_lazy/.config/nvim/lua/plugins/navigation.lua
+++ nvim_lazy/.config/nvim/lua/plugins/navigation.lua
@@ -6,6 +6,10 @@ local M = {
 
 function M.config()
     require("leap").add_default_mappings()
+
+    vim.api.nvim_set_hl(0, "LeapMatch", { fg = '#2e3440', bg = '#a3be8c' })
+    vim.api.nvim_set_hl(0, "LeapLabelPrimary", { fg = '#2e3440', bg = "#a3be8c" })
+    vim.api.nvim_set_hl(0, "LeapLabelSecondary", { fg = '#2e3440', bg = "#ebcb8b" })
 end
 
 return M
